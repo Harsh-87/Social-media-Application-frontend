ea47a3f1630dc845b5e539e4e2d0fec22539ea47
Harsh Kothari <harsh.kothari@hyperverge.co>
2021-01-14 21:53:34 +0530

    Assets folder added ,Images and styles files location modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './assets/styles/App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import NavBar from './components/layout/Navbar'
